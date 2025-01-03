92e2b2404a543d0d6faecc9b26f7f3e4e1e878b3
Marius-Vlad Dumitru <marius_vlad.dumitru@stud.etti.upb.ro>
2025-01-03 22:36:25 +0200

    Update.

--- main.py
+++ main.py
@@ -1,101 +1,115 @@
 from os import listdir
-import csv
-
-
-dataset_root = 'dataset'
-filenames = listdir(dataset_root)
-csv_file = None
-for filename in filenames:
-    if 'csv' in filename:
-        csv_file = filename
-        break
-addnotations = []
-with open (dataset_root + '/' + csv_file, newline='') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        addnotations.append(row)
-
-print(addnotations)
-# This may be deleted in the future
-# Positions in these lists are correlated with each other: each position is the index of the row in the csv file
-# Ex: FileName[x] is correlated with GameType[x], Map[x], Player1["IP"][x], Player1["Port"][x], Player1["Bot_Difficulty"][x], Player1["Bot_Behavior"][x], Player1["Types"][x] Player2["IP"][x], Player2["Port"][x], Player2["Bot_Difficulty"][x], Player2["Bot_Behavior"][x], Player2["Types"][x], Server[x], Client[x], WhoIsMe[x]
-FileName = []
-GameType = []
-Map = []
-Player1 = {"IP": [], "Port": [], "Types": [], "Bot_Difficulty": [] , "Bot_Behavior": []}
-Player2 = {"IP": [], "Port": [], "Types": [], "Bot_Difficulty": [] , "Bot_Behavior": []}
-Server = []
-Client = []
-WhoIsMe = []
-Observation = []
-
-for idx, value in enumerate(addnotations):
-    if idx == 0:
-        continue
-    FileName.append(value[0])
-    GameType.append(value[1])
-    Map.append(value[2])
-    Player1["IP"].append(value[3].split(":")[0])
-    Player1["Port"].append(value[3].split(":")[1])
-    Player2["IP"].append(value[4].split(":")[0])
-    Player2["Port"].append(value[4].split(":")[1])
-    Server.append(value[5])
-    Client.append(value[6])
-    WhoIsMe.append(value[7])
-    Player1["Types"].append(value[8].split(":")[0])
-    Player2["Types"].append(value[8].split(":")[1])
-    Player1["Bot_Difficulty"].append(value[9].split(":")[0])
-    Player2["Bot_Difficulty"].append(value[9].split(":")[1])
-    Player1["Bot_Behavior"].append(value[10].split(":")[0])
-    Player2["Bot_Behavior"].append(value[10].split(":")[1])
-    Observation.append(value[11])
-
-# check if lists are correct
-# for idx, value in enumerate(addnotations):
-#     if idx == 0:
-#         continue
-#     print("CSV ROW: ", idx, "Value:", value)
-#     print("Filename:", FileName[idx - 1], "GameType:", GameType[idx - 1], "Map:", Map[idx - 1], "Player1:", "IP:", Player1["IP"][idx - 1], "Port:",  Player1["Port"][idx - 1], "Types:", Player1["Types"][idx - 1], "Bot_Difficulty:", Player1["Bot_Difficulty"][idx - 1], "Bot_Behavior", Player1["Bot_Behavior"][idx - 1], "Player2:", "IP:", Player2["IP"][idx - 1], "Port:", Player2["Port"][idx - 1], "Types:", Player2["Types"][idx - 1], "Bot_Difficulty:", Player2["Bot_Difficulty"][idx - 1], "Bot_Behavior:", Player2["Bot_Behavior"][idx - 1], "Server:", Server[idx - 1], "Client:", Client[idx - 1], "WhoIsMe:", WhoIsMe[idx - 1], "Observation:", Observation[idx - 1], '\n')
-
 from scapy.all import rdpcap
 from scapy.layers.inet import IP, UDP
 from scapy.packet import Raw
+from scapy.all import PcapReader
+import sys
+import csv
+import random
 
-packets = rdpcap(dataset_root + '/' + FileName[0])
+def get_addnotations():
+    """
+    Input None
+    Output: Data from addnontations.csv as a list
+    Get Addnotation data from addnotations.csv
+    """
+    dataset_root = 'dataset'
+    filenames = listdir(dataset_root)
+    csv_file = None
+    for filename in filenames:
+        if 'csv' in filename:
+            csv_file = filename
+            break
+    addnotations = []
+    with open (dataset_root + '/' + csv_file, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            addnotations.append(row)
 
-# Iterate over packets and print a summary
-# for pkt in packets:
-#     print(pkt.summary())
+    return addnotations
+
+def parse_capture_file(capture_file):
+    addnotations = get_addnotations()
+
+    # find capture_file. Assuming that a valid capture file is always provided
+    for idx, data in enumerate(addnotations):
+        if idx == 0:
+            continue
+        if capture_file == data[0]: # found the needed capture file
+            capture_file_data = data
+            break
+
+    Player1 = {"IP": "", "Port": "", "Type": int()}
+    Player2 = {"IP": "", "Port": "", "Type": int()}
+    Player1["IP"] = capture_file_data[3].split(":")[0]
+    Player1["Port"] = int(capture_file_data[3].split(":")[1])
+    Player2["IP"] = capture_file_data[4].split(":")[0]
+    Player2["Port"] = int(capture_file_data[4].split(":")[1])
+
+    # class Human is encoded as class 0
+    # class Bot is encoded as class 1
+    Player1["Type"] = 0 if capture_file_data[8].split(":")[0] == "Human" else 1
+    Player2["Type"] = 0 if capture_file_data[8].split(":")[1] == "Human" else 1
+
+    # valid ip's and ports
+    valid_ips = [Player1["IP"], Player2["IP"]]
+    valid_ports = [Player1["Port"], Player2["Port"]]
+
+    # # network stuff
+    with PcapReader('dataset' + '/' + capture_file) as pcap_reader:
+        for pkt in pcap_reader:
+            if IP in pkt and UDP in pkt:
+                ip_layer = pkt[IP]
+                udp_layer = pkt[UDP]
+                src_ip = ip_layer.src
+                src_port = udp_layer.sport
+                dst_ip = ip_layer.dst
+                dst_port = udp_layer.dport
+
+                if src_port not in valid_ports or dst_port not in valid_ports:
+                    continue
+                if src_ip not in valid_ips or dst_ip not in valid_ips:
+                    continue
+
+                # The packet has a valid port and a valid ip
+                if Raw in pkt:
+                    udp_packet_data = pkt[Raw].load # Has the ENet Header + 0.AD payload data
+                    udp_packet_bytes = len(udp_packet_data)
+                    for idx in range(0, udp_packet_bytes):
+                        print(udp_packet_data[idx], end='   ')
+                    print("END_PACKET")
 
 
-# for pkt in packets:
-#     # Check if this packet has an IP layer
-#     if IP in pkt:
-#         src_ip = pkt[IP].src
-#         dst_ip = pkt[IP].dst
-#
-#         print(f"Source IP: {src_ip} --> Destination IP: {dst_ip}")
+# get filelist of capture directory
+dataset_root = 'dataset'
+capture_files = listdir(dataset_root)
+for filename in capture_files:
+    if '.pcapng' not in filename:
+        capture_files.remove(filename)
 
-for pkt in packets:
-    # Check if the packet has IP and UDP layers
-    if IP in pkt and UDP in pkt:
-        ip_layer = pkt[IP]
-        udp_layer = pkt[UDP]
+# parse capture files
+for capture_file in capture_files:
+    parse_capture_file(capture_file)
+    # aici faci vectorul mare de trasaturi care va fi trimis catre model. la vectoru asta contribuie fiecare capture file
 
-        src_ip = ip_layer.src
-        dst_ip = ip_layer.dst
-        src_port = udp_layer.sport
-        dst_port = udp_layer.dport
 
-        print(f"UDP Packet: {src_ip}:{src_port} --> {dst_ip}:{dst_port}")
+# X - matricea ce contine datele
+# Y - matricea ce contine addnotarile (label-urile) - clasele
+# OUT - matricea ce contine ceea ce a prezis modelul
+#X_train, Y_train - pentru antrenare
+# X_val, Y_val - pentru validare
+# OUT_train OUT_val - ceea ce a prezis modelul boss, pentru antrenare si validare
 
-        # If there's raw payload data, print it
-        if Raw in pkt:
-            raw_data = pkt[Raw].load
-            print(f"Payload (Raw Data): {raw_data}")
 
-        print("-" * 50)
+# dataset = capture_files
+# random.seed()
+# random.shuffle(dataset)
 
-    # DIN PACHETE TREBUIE SA EXTRAGI TRASATURI BOSS
-    # TRE SA VEZI MAI INTAI CARE SUNT TRASATURILE SI APOI CUM LE EXTRAGI DIN TRAFIC
-    # AI DE PLM AGAIN
+# 80 / 20 dataset split
+# train_idx = round(len(dataset) * 0.8)
+# dataset_train = dataset[0:train_idx]
+# dataset_val = dataset[train_idx:]
+
+# DIN PACHETE TREBUIE SA EXTRAGI TRASATURI BOSS
+# TRE SA VEZI MAI INTAI CARE SUNT TRASATURILE SI APOI CUM LE EXTRAGI DIN TRAFIC
+# AI DE PLM AGAIN
